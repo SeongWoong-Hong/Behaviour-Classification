6d88e32a18df75f8d881c1557eee71c3e2b8f305
sw.hong <sw.hong@kaist.ac.kr>
2021-01-26 14:45:49 +0900

    refactor: Add explanation for the input parameters

--- Models.py
+++ Models.py
@@ -1,8 +1,13 @@
 import torch
 import torch.nn as nn
 
+
 class ANNModel(nn.Module):
-    def __init__(self, inp, oup):
+    def __init__(self, inp: int, oup: int):
+        """
+        :param inp: (int) the size of inputs
+        :param oup: (int) the number of labels of data
+        """
         super(ANNModel, self).__init__()
         self.layer1 = nn.Linear(inp, 128)
         self.layer2 = nn.Linear(128, oup)
@@ -13,10 +18,13 @@ class ANNModel(nn.Module):
         out = self.relu(self.layer2(x))
         return out
 
+
 class CNNModel(nn.Module):
-    #inp: total number of input image pixels. (channel * width * height)
-    #oup: total number of labels.
-    def __init__(self, inp, oup):
+    def __init__(self, inp: int, oup: int):
+        """
+        :param inp: (int) the total number of the input image pixels (channel * width * height)
+        :param oup: (int) the number of labels
+        """
         super(CNNModel, self).__init__()
         lst_ch = 4
         self.conv1 = nn.Conv2d(1, 4, kernel_size=3, padding=1)
@@ -33,6 +41,8 @@ class CNNModel(nn.Module):
         out = self.linear(out)
         return out
 
+
+# Not implemented yet
 class RNNModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(RNNModel, self).__init__()
