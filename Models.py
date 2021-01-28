18d2855319b5b06632520a8e9469621721f0406b
sw.hong <sw.hong@kaist.ac.kr>
2021-01-28 15:34:05 +0900

    feat: Add 'BaseModel' class Make the 'BaseModel' class for the all network models that have 'fit' and 'test' methods for fitting and testing.

--- Models.py
+++ Models.py
@@ -1,17 +1,69 @@
+from typing import Type
+
+import numpy as np
 import torch
 import torch.nn as nn
 
 
-class ANNModel(nn.Module):
-    def __init__(self, inp: int, oup: int):
+class BaseModel(nn.Module):
+    def fit(self, train_loader):
+        self.train()
+        device = next(self.parameters()).device.index
+        losses = []
+        for i, data in enumerate(train_loader):
+            image = data[0].type(torch.FloatTensor).cuda(device)
+            label = data[1].type(torch.LongTensor).cuda(device)
+
+            pred_label = self.forward(image)
+            loss = self.criterion(pred_label, label)
+            losses.append(loss.item())
+
+            self.optim.zero_grad()
+            loss.backward()
+            self.optim.step()
+        avg_loss = sum(losses) / len(losses)
+        return avg_loss
+
+    def test(self, test_loader):
+        self.eval()
+        device = next(self.parameters()).device.index
+        pred_labels = []
+        real_labels = []
+        for i, data in enumerate(test_loader):
+            image = data[0].type(torch.FloatTensor).cuda(device)
+            label = data[1].type(torch.LongTensor).cuda(device)
+            real_labels += list(label.cpu().detach().numpy())
+
+            pred_label = self.forward(image)
+            pred_label = list(pred_label.cpu().detach().numpy())
+            pred_labels += pred_label
+
+        real_labels = np.array(real_labels)
+        pred_labels = np.array(pred_labels)
+        pred_labels = pred_labels.argmax(axis=1)
+        acc = sum(real_labels == pred_labels) / len(real_labels) * 100
+        return acc, pred_labels, real_labels
+
+
+class ANNModel(BaseModel):
+    def __init__(self,
+                 inp: int,
+                 oup: int,
+                 lr: float = 1e-4,
+                 momentum: float = 0.9,
+                 optim: Type[torch.optim.Optimizer] = torch.optim.Adam,
+                 criterion=nn.CrossEntropyLoss):
         """
         :param inp: (int) the size of inputs
         :param oup: (int) the number of labels of data
+        :param lr: (float) learning rate
         """
         super(ANNModel, self).__init__()
         self.layer1 = nn.Linear(inp, 128)
         self.layer2 = nn.Linear(128, oup)
         self.relu = nn.ReLU(inplace=True)
+        self.optim = optim(self.parameters(), lr=lr, momentum=momentum)
+        self.criterion = criterion()
 
     def forward(self, x):
         x = self.relu(self.layer1(x))
@@ -19,19 +71,29 @@ class ANNModel(nn.Module):
         return out
 
 
-class CNNModel(nn.Module):
-    def __init__(self, inp: int, oup: int):
+class CNNModel(BaseModel):
+    def __init__(self,
+                 inp: int,
+                 oup: int,
+                 lst_ch: int,
+                 lr: float = 1e-4,
+                 momentum: float = 0.9,
+                 optim: Type[torch.optim.Optimizer] = torch.optim.Adam,
+                 criterion=nn.CrossEntropyLoss
+                 ):
         """
         :param inp: (int) the total number of the input image pixels (channel * width * height)
         :param oup: (int) the number of labels
+        :param lr: (float) learning rate
         """
         super(CNNModel, self).__init__()
-        lst_ch = 4
         self.conv1 = nn.Conv2d(1, 4, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(4, 16, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(16, lst_ch, kernel_size=3, padding=1)
         self.linear = nn.Linear(inp * lst_ch, oup)
         self.relu = nn.ReLU(inplace=True)
+        self.optim = optim(self.parameters(), lr=lr, momentum=momentum)
+        self.criterion = criterion()
 
     def forward(self, x):
         out = self.relu(self.conv1(x))
@@ -52,7 +114,7 @@ class RNNModel(nn.Module):
         self.len = 1
 
     def forward(self, x):
-        packed = nn.utils.rnn.pack_padded_sequence(x, self.len)
+        # packed = nn.utils.rnn.pack_padded_sequence(x, self.len)
         output, (hidden, cell) = self.lstm(x)
         out = self.relu(self.fc(hidden.squeeze(0)))
         return out
