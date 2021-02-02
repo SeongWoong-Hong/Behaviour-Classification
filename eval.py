18e8cc03c7ea564ea450135121c1dfee6400e665
sw.hong <sw.hong@kaist.ac.kr>
2021-02-02 16:49:30 +0900

    style: Minor modification

--- eval.py
+++ eval.py
@@ -2,7 +2,6 @@ import csv
 import torch
 
 import numpy as np
-import torch.nn as nn
 from torch.utils.data import DataLoader
 
 testdata = csv.reader(open("test.txt"), delimiter='\t')
@@ -18,10 +17,7 @@ for line in testdata:
     test_data.append([float(i) for i in line[0:-1]])
 
 ##
-BATCH_SIZE = 128
-
-##
-test_data = np.array(test_data, dtype=np.float32)
+test_data = np.array(test_data, dtype=np.float64)
 test_image = test_data.reshape([len(test_label), 1, -1, 6])
 label_data = np.array(test_label, dtype=np.int)
 inp = test_data.shape[1]
@@ -33,14 +29,17 @@ tensor_label_data = torch.from_numpy(label_data)
 test_dataset = list(zip(tensor_test_data, tensor_label_data))
 test_imgset = list(zip(tensor_test_image, tensor_label_data))
 
-test_data_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-test_img_loader = DataLoader(dataset=test_imgset, batch_size=BATCH_SIZE, shuffle=False)
+test_data_loader = DataLoader(dataset=test_dataset, batch_size=len(test_dataset), shuffle=False)
+test_img_loader = DataLoader(dataset=test_imgset, batch_size=len(test_imgset), shuffle=False)
 
 ANNet = torch.load("./ANN.pt")
 CNNet = torch.load("./CNN.pt")
 
 Acc1, pred_labels_ann, real_labels1 = ANNet.test(test_data_loader)
 Acc2, pred_labels_cnn, real_labels2 = CNNet.test(test_img_loader)
+
+print("ANN model predict accuracy is {}".format(Acc1))
+print("CNN model predict accuracy is {}".format(Acc2))
 
 f1 = open("pred_labels_ann.txt", "w", encoding='ascii', newline='')
 f2 = open("pred_labels_cnn.txt", "w", encoding='ascii', newline='')
