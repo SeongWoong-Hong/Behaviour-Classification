97f5825c24d4aa161b18f687e50bf3f2a9374685
sw.hong <sw.hong@kaist.ac.kr>
2021-02-03 16:59:58 +0900

    style: Change lines for loading and saving data

--- eval.py
+++ eval.py
@@ -1,30 +1,19 @@
-import csv
 import torch
 
-import numpy as np
+from scipy import io
 from torch.utils.data import DataLoader
 
-testdata = csv.reader(open("test.txt"), delimiter='\t')
-testlabel = csv.reader(open("test_label.txt"), delimiter='\t')
 
-test_label = []
-test_data = []
-
-for line in testlabel:
-    test_label.append(int(float(line[0])))
-
-for line in testdata:
-    test_data.append([float(i) for i in line[0:-1]])
+test_data = io.loadmat('test.mat')['Testwindow']
+test_label = io.loadmat('test.mat')['TestLabel'].ravel()
 
 ##
-test_data = np.array(test_data, dtype=np.float64)
 test_image = test_data.reshape([len(test_label), 1, -1, 6])
-label_data = np.array(test_label, dtype=np.int)
 inp = test_data.shape[1]
 
 tensor_test_data = torch.from_numpy(test_data)
 tensor_test_image = torch.from_numpy(test_image)
-tensor_label_data = torch.from_numpy(label_data)
+tensor_label_data = torch.from_numpy(test_label)
 
 test_dataset = list(zip(tensor_test_data, tensor_label_data))
 test_imgset = list(zip(tensor_test_image, tensor_label_data))
@@ -41,15 +30,5 @@ Acc2, pred_labels_cnn, real_labels2 = CNNet.test(test_img_loader)
 print("ANN model predict accuracy is {}".format(Acc1))
 print("CNN model predict accuracy is {}".format(Acc2))
 
-f1 = open("pred_labels_ann.txt", "w", encoding='ascii', newline='')
-f2 = open("pred_labels_cnn.txt", "w", encoding='ascii', newline='')
-wr1 = csv.writer(f1)
-wr2 = csv.writer(f2)
-
-for param in pred_labels_ann:
-    wr1.writerow([param, ''])
-f1.close()
-
-for param in pred_labels_cnn:
-    wr2.writerow([param, ''])
-f2.close()
+io.savemat('pred_labels_ann.mat', {'pred_labels_ann': pred_labels_ann})
+io.savemat('pred_labels_cnn.mat', {'pred_labels_cnn': pred_labels_cnn})
